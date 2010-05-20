d197c55847193bd31bde8e30dc9a7da6bba30e33
Dwi Widiastuti <admin@diazuwi.web.id>
2010-05-20 23:26:06 +0700

    use DS instead of slash (OS compat)

--- webapp/model/class.SmartyInstaller.php
+++ webapp/model/class.SmartyInstaller.php
@@ -3,7 +3,7 @@ class SmartyInstaller extends Smarty {
   function SmartyInstaller() {
     $this->Smarty();
     $this->template_dir = array(THINKTANK_WEBAPP_PATH . 'view');
-    $this->compile_dir = THINKTANK_WEBAPP_PATH . 'view/compiled_view/';
+    $this->compile_dir = THINKTANK_WEBAPP_PATH . 'view' . DS . 'compiled_view' . DS;
     $this->caching = FALSE;
   }
 }
