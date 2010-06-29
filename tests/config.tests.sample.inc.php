1c6bf04aa79ce63bc126ad8e1088c2a21243b6e7
Dwi Widiastuti <admin@diazuwi.web.id>
2010-06-29 11:11:03 +0700

    add test database credential and site root path in config test

--- tests/config.tests.sample.inc.php
+++ tests/config.tests.sample.inc.php
@@ -2,9 +2,17 @@
 
 $SOURCE_ROOT_PATH = "/your-path-to/thinktank/";
 
+$SITE_ROOT_PATH = "/";
+
 $TEST_SERVER_DOMAIN = 'http://localhost';
 
 $TEST_DATABASE = 'thinktank_tests';
+
+$TEST_DATABASE_USER = 'root';
+
+$TEST_DATABASE_PASSWORD = '123456';
+
+$TEST_DATABASE_HOST = 'localhost';
 
 $INCLUDE_PATH = "/your-path-to/thinktank/webapp/";
 
